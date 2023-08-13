e3f2f40514cd9b206936616678098656fae731c1
someone-riy <someonewholikescoffe08@gmail.com>
2023-08-13 23:29:02 +0800

    adding little features and some changes

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,8 +1,9 @@
-const { bcrypt } = require('bcrypt');
 const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
+const { bcrypt } = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
   constructor() {
@@ -36,7 +37,7 @@ class UsersService {
 
     const result = await this._pool.query(query);
 
-    if (result.rows.length > 0) {
+    if (result.rowCount > 0) {
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan');
     }
   }
@@ -54,6 +55,29 @@ class UsersService {
     }
 
     return result.rows[0];
+  }
+
+  async verrifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    return id;
   }
 }
 
